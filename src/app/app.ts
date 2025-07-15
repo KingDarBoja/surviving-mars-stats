5fea9a701e4ffb996f80d153e58e26801ca04298
KingDarBoja <stevenbojato04@gmail.com>
2025-07-15 12:35:33 -0500

    feat: add some light and standard material.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,16 @@
 import { ChangeDetectionStrategy, Component, input } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NgtCanvas } from 'angular-three/dom';
-import { NgtCameraParameters } from 'angular-three';
+import { NgtCameraParameters, NgtVector3 } from 'angular-three';
 
 import { MarsScene } from './mars-scene/mars-scene.component';
 
+/**
+ * In order to render your scene graph properly, Angular Three needs to wait
+ * until it finishes setting up the context (i.e: store) before rendering. This
+ * is where `NgtCanvasContent` directive comes in. It acts purely as an anchor
+ * so `NgtCanvas` can render the content when it is ready.
+ */
 @Component({
   imports: [RouterModule, NgtCanvas, MarsScene],
   standalone: true,
@@ -17,7 +23,8 @@ import { MarsScene } from './mars-scene/mars-scene.component';
 
     <div id="canvas-container">
       <ngt-canvas
-        [camera]="canvasCamera()"
+        [camera]="canvasCamera"
+        [lookAt]="canvasLookAt"
         (created)="$event.gl.setClearColor('black')"
       >
         <sms-mars-scene *canvasContent />
@@ -29,7 +36,8 @@ import { MarsScene } from './mars-scene/mars-scene.component';
 export class App {
   protected title = 'surviving-mars-stats';
 
-  canvasCamera = input<NgtCameraParameters>({
-    position: [0, 0, 20],
-  });
+  protected canvasCamera: NgtCameraParameters = {
+    position: [0, 0, 18],
+  };
+  protected canvasLookAt: NgtVector3 = [0, 0, 0];
 }
--- src/app/mars-scene/mars-scene.component.ts
+++ src/app/mars-scene/mars-scene.component.ts
@@ -9,16 +9,18 @@ import {
 import { beforeRender, extend, NgtArgs, NgtVector3 } from 'angular-three';
 import {
   Color,
+  HemisphereLight,
   Mesh,
-  MeshBasicMaterial,
-  MeshBasicMaterialParameters,
+  MeshStandardMaterial,
+  MeshStandardMaterialParameters,
   SphereGeometry,
 } from 'three';
 
-extend({ Color, Mesh, SphereGeometry, MeshBasicMaterial });
+extend({ Color, Mesh, SphereGeometry, MeshStandardMaterial });
 
 /**
- * - `CUSTOM_ELEMENTS_SCHEMA` is required to use Angular Three components.
+ * - `CUSTOM_ELEMENTS_SCHEMA` is required to use Angular Three elements in the
+ *   template.
  */
 @Component({
   selector: 'sms-mars-scene',
@@ -27,28 +29,50 @@ extend({ Color, Mesh, SphereGeometry, MeshBasicMaterial });
   changeDetection: ChangeDetectionStrategy.OnPush,
   imports: [NgtArgs],
   template: `
-    <ngt-color attach="background" *args="['#303030']" />
+    <ngt-color attach="background" *args="sceneColorArgs" />
+    <ngt-hemisphere-light *args="hemiLightArgs" />
 
-    <ngt-mesh #sphereMeshRef [position]="meshPosition()">
+    <ngt-mesh #sphereMeshRef [position]="meshPosition">
       <ngt-sphere-geometry
         *args="sphereGeoArgs"
         (attached)="onAttachSphere($event)"
       />
 
-      <ngt-mesh-basic-material [parameters]="meshMatParams" />
+      <ngt-mesh-standard-material [parameters]="meshMatParams" />
     </ngt-mesh>
   `,
 })
 export class MarsScene {
-  protected meshPosition = input<NgtVector3>(0);
-  protected meshMatParams: MeshBasicMaterialParameters = {
-    color: '#bd5417',
+  /* -------------------- SCENE RELATED CONFIG -------------------- */
+
+  /** Convert from Hex to RGB to avoid typing errors. */
+  protected sceneColor = new Color().setHex(0x303030);
+  /** NgtArgs accepts an array of Constructor Arguments that the entity accepts.
+   * In this case, it requires a triplet. */
+  protected sceneColorArgs: ConstructorParameters<typeof Color> = [
+    this.sceneColor.r,
+    this.sceneColor.g,
+    this.sceneColor.b,
+  ];
+
+  protected hemiLightArgs: ConstructorParameters<typeof HemisphereLight> = [
+    0xffffff, 0x808080,
+  ];
+
+  /* -------------------- MESH RELATED CONFIG -------------------- */
+  protected meshPosition: NgtVector3 = [0, 0, 0];
+  protected meshMatParams: MeshStandardMaterialParameters = {
+    color: 0xbd5417, // Orange-redish
     wireframe: false,
+    flatShading: false,
   };
 
+  /* -------------------- GEOMETRY RELATED CONFIG -------------------- */
   protected sphereGeoArgs: ConstructorParameters<typeof SphereGeometry> = [
-    8, 64, 64,
+    8, 32, 32,
   ];
+
+  /* -------------------- ANIMATION RELATED CONFIG -------------------- */
 
   /** Obtain the mesh reference with `viewChild` signal. */
   sphereMeshRef = viewChild.required<ElementRef<Mesh>>('sphereMeshRef');
@@ -58,6 +82,9 @@ export class MarsScene {
     console.log('Parent: ', parent);
   }
 
+  /** Mars axis tilt angle in radians. */
+  private readonly _marsTiltAngle = -(25.2 * Math.PI) / 180;
+
   /** Add the animation to the Three-js loop with `beforeRender` */
   constructor() {
     beforeRender(() => {
@@ -66,11 +93,12 @@ export class MarsScene {
   }
 
   /**
-   * Perform all animation related configurations here.
+   * Perform all animation related to our sphere here.
    */
   private animateSphere() {
     const sphereMeshEl = this.sphereMeshRef().nativeElement;
-    sphereMeshEl.rotateX(0.0005);
-    sphereMeshEl.rotateY(0.005);
+    /** The tilt angle. */
+    sphereMeshEl.rotation.z = this._marsTiltAngle;
+    sphereMeshEl.rotateY(0.0001);
   }
 }
